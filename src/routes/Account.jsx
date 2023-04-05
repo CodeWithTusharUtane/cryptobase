c621df189ef3040b16b8431c5714574bcd089453
unknown <tusharutane2@gmail.com>
2023-04-05 12:24:45 +0530

    added firebase for authentication and firestore database

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -3,10 +3,10 @@ import SavedCoin from '../components/SavedCoin'
 
 const Account = () => {
   return (
-    <div>
-      <div>
+    <div className='mx-w-[1140px] mx-auto'>
+      <div className='flex justify-between items-center my-12 py-8 rounded-div'>
         <div>
-          <h1>
+          <h1 className='text-2xl font-bold'>
             Account
           </h1>
           <div className="">
@@ -14,12 +14,12 @@ const Account = () => {
           </div>
         </div>
         <div>
-        <button>Sign Out</button>
+        <button className='border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl'>Sign Out</button>
         </div>
       </div>
-      <div>
-        <div>
-        <h1>Watch List</h1>
+      <div className='flex justify-between items-center my-12 py-8 rounded-div'>
+        <div className='w-full min-h-[300px]'>
+        <h1 className='text-2xl font-bold py-4'>Watch List</h1>
           <SavedCoin/>
         </div>
       </div>
