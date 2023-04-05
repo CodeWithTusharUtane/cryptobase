95747b6360b411f5540672a23181d037d35512de
unknown <tusharutane2@gmail.com>
2023-04-05 11:50:24 +0530

    Added account, signup and saved coin page

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import SavedCoin from '../components/SavedCoin'
 
 const Account = () => {
   return (
-    <div>Account</div>
+    <div>
+      <div>
+        <div>
+          <h1>
+            Account
+          </h1>
+          <div className="">
+            <p>Welcome, user</p>
+          </div>
+        </div>
+        <div>
+        <button>Sign Out</button>
+        </div>
+      </div>
+      <div>
+        <div>
+        <h1>Watch List</h1>
+          <SavedCoin/>
+        </div>
+      </div>
+    </div>
   )
 }
 
